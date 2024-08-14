3fb8b33078a2e4554e9a62063e67398cc0e898fb
CSAN7690 <christophersanchez@pursuit.org>
2024-08-14 13:24:22 -0400

    Was able to pass test and show my data on FrontEnd as shown in example screenshot! 🫡

--- frontend/src/Components/Animes.js
+++ frontend/src/Components/Animes.js
@@ -1,11 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import Anime from "./Anime";
 
 function Animes() {
   //fetch all animes from the backend and render them as a list using the Anime component. Make sure to style the animes to look like the screenshot from the README. Feel free to use axios to grab data
 
+  const [animes, setAnimes] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = `${process.env.REACT_APP_BASE_URL}/animes`;
+
+    fetch(apiUrl)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return res.json()
+      })
+      .then(data => {
+        setAnimes(data)
+      })
+      .catch(error => {
+        console.error('Error fetching animes:', error)
+      })
+  }, [])
   return (
     <section className="index" id="anime-list">
-      <p>No animes to display</p>
+      {animes.length > 0 ? (
+        animes.map(anime => (
+          <Anime key={anime.id} name={anime.name} description={anime.description} />
+        ))
+      ) : (
+        <p>No animes to display</p>
+      )}
     </section>
   );
 }
